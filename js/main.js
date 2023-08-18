f237e96d40ce3e43c59d7da90437e645ef8d86d5
tamheedaakas <tamheed.samar@gmail.com>
2023-08-18 20:33:31 +0500

    completed site

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll= function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }
+    else{
+        nav.classList.remove("scroll-on"); 
+    }
+}
+//NAV HIDE
+let navbar= document.querySelectorAll('.nav-link');
+let navCollapse= document.querySelector('.navbar-collapse.collapse');
+navbar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show"); 
+    })
+})
+//counter design
+document.addEventListener("DOMContentLoaded",()=>{
+    function counter(id, start, end, duration){
+        let obj=document.getElementById(id),
+        current=start,
+        range=end - start,
+        increment= end > start ? 1 : -1,
+        step= Math.abs(Math.floor(duration / range)),
+        timer= setInterval(()=>{
+            current += increment;
+            obj.textContent = current;
+            if(current==end){
+                clearInterval(timer)
+            }
+        },step)
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 0, 5284, 2500);
+    counter("count3", 0, 3283, 3000);
+    counter("count4", 0, 4237, 3000);
+})
